deb28f9b36120ee1e1f1867307a7e66917e5c4d3
jatin <jatin2372005@gmail.com>
2026-08-15 00:46:08 +0530

    will be coding the backend from scratch as there are many changes

--- Website_Code/Frontend/src/App.tsx
+++ Website_Code/Frontend/src/App.tsx
@@ -1,36 +1,122 @@
-import { Routes, Route } from "react-router-dom";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
 
-import ProtectedRoutes from "./context/protected_routes";
-import { AuthProvider } from "./context/auth_context";
+function App() {
+  const [count, setCount] = useState(0)
 
-import Layout from "./components/layout";
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+        <div>
+          <h1>Get started</h1>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+        <button
+          type="button"
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+      </section>
 
-import Setup from "./pages/setup";
-import Auth from "./pages/auth";
-import Intro from "./pages/intro";
-import Dashboard from "./pages/dashboard";
-import Profile from "./pages/profile";
-import Projects from "./pages/projects";
-import ProjectDashboard from "./pages/project";
+      <div className="ticks"></div>
 
-export default function App() {
-    return (
-        <AuthProvider>
-            <Routes>
-                <Route path="/" element={<Intro />} />
-                <Route path="/auth" element={<Auth />} />
+      <section id="next-steps">
+        <div id="docs">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#documentation-icon"></use>
+          </svg>
+          <h2>Documentation</h2>
+          <p>Your questions, answered</p>
+          <ul>
+            <li>
+              <a href="https://vite.dev/" target="_blank">
+                <img className="logo" src={viteLogo} alt="" />
+                Explore Vite
+              </a>
+            </li>
+            <li>
+              <a href="https://react.dev/" target="_blank">
+                <img className="button-icon" src={reactLogo} alt="" />
+                Learn more
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div id="social">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#social-icon"></use>
+          </svg>
+          <h2>Connect with us</h2>
+          <p>Join the Vite community</p>
+          <ul>
+            <li>
+              <a href="https://github.com/vitejs/vite" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#github-icon"></use>
+                </svg>
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a href="https://chat.vite.dev/" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#discord-icon"></use>
+                </svg>
+                Discord
+              </a>
+            </li>
+            <li>
+              <a href="https://x.com/vite_js" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#x-icon"></use>
+                </svg>
+                X.com
+              </a>
+            </li>
+            <li>
+              <a href="https://bsky.app/profile/vite.dev" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#bluesky-icon"></use>
+                </svg>
+                Bluesky
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
 
-                <Route element={<ProtectedRoutes />}>
-                    <Route element={<Layout />}>
-                        <Route path="/dashboard" element={<Dashboard />} />
-                        <Route path="/docs" element={<Setup />} />
-                        <Route path="/profile" element={<Profile />} />
-                        <Route path="/projects" element={<Projects />} />
-                        <Route path="/projects/:projectId" element={<ProjectDashboard />} />
-                        <Route path="/createprofile" element={<Profile />} />
-                    </Route>
-                </Route>
-            </Routes>
-        </AuthProvider>
-    );
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
 }
+
+export default App
--- Website_Code/backend/Models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .auth_model import Auth_User, Personal_Data
-from .project_model import Project_Events, Projects
-from .embedding_model import *
-from .incidents_model import *
